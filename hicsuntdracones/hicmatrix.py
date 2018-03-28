9889a49443589f9707e76a810fab5af9ed539157
Konrad Förstner <konrad@foerstner.org>
2018-03-28 12:54:23 +0200

    Add subcommand to normalize by column sums

--- hicsuntdracones/hicmatrix.py
+++ hicsuntdracones/hicmatrix.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import pandas as pd
 
 
@@ -27,8 +28,28 @@ class HiCMatrix():
                              "Is this really a HiC matrix in Homer format?")
             sys.exit(1)
     
-    def to_csv():
-        pass
+    def save(self, output_hic_matrix_file):
+        self.hic_matrix_df.to_csv(output_hic_matrix_file, sep="\t",
+                                  index=False)
+
+    def normalize_by_columns_sum(self):
+        """
+        Assumption: input is an iced matrix.
+        """
+        column_sum_median = self._calc_column_sum_median()
+        for column in self.hic_matrix_df.columns:
+            if column in ["HiCMatrix", "Regions"]:
+                continue
+            self.hic_matrix_df[column] = self.hic_matrix_df[
+                column] / column_sum_median
+
+    def _calc_column_sum_median(self):
+        """Needed as the sums are not completely identical and some row sums
+        are 0. Assumption: input is an iced matrix.
+        """
+        return np.median([
+            self.hic_matrix_df[col].sum()
+            for col in self.hic_matrix_df.columns[2:]])
 
     def _chromosomes(self):
         return sorted(self.hic_matrix_df[
@@ -39,4 +60,4 @@ def remove_position_information(name_with_pos_info):
     # Return just the chromosome part without the exact window
     # location
     return "-".join(name_with_pos_info.split("-")[:-1])
-    
+
--- tests/test_hicmatrix.py
+++ tests/test_hicmatrix.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import hicsuntdracones.hicmatrix
 
 
@@ -8,3 +9,21 @@ def test_matrix_generation():
     assert hic_matrix.hic_matrix_df.shape == (12, 14)
     assert hic_matrix.number_of_bins == 12
     assert hic_matrix.chromosomes == ["chr1", "chr2"]
+
+
+def test_normalize_by_columns_sum():
+    hic_matrix = hicsuntdracones.hicmatrix.HiCMatrix(
+        "tests/fixtures/homer_matrix_small.txt")
+    assert hic_matrix._calc_column_sum_median() == 20.0
+    hic_matrix.normalize_by_columns_sum()
+    pd.testing.assert_frame_equal(
+        pd.DataFrame.from_dict(
+            {"HiCMatrix": [
+                "chr1-0", "chr1-10000", "chr2-0", "chr2-10000"],
+             "Regions": [
+                 "chr1-0", "chr1-10000", "chr2-0", "chr2-10000"],
+             "chr1-0": [0.50, 0.15, 0.0, 0.35],
+             "chr1-10000": [0.15, 0.65, 0.0, 0.20],
+             "chr2-0": [0.0, 0.0, 0.0, 0.0],
+             "chr2-10000": [0.35, 0.20, 0.00, 0.45]}),
+        hic_matrix.hic_matrix_df)
